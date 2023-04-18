a9f3c801b16a61b5a19d95e2b045c71c7c490d0b
Damian Silbergleith <dsilbergleith.github@gmail.com>
2023-04-18 06:27:25 -0700

    add working code and a readme

---
+++ README.md
@@ -0,0 +1,4 @@
+# Lesson 4 RGB
+This is a simple bit of code that cycles through the colors of an RGB LED.
+
+THis is lesson 4 in the Elegoo starter kit.
--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,50 @@
 #define GREEN 5
 #define BLUE 6
 
+#define DELAY 10
+#define DELAY_BETWEEN_LIGHTS 250
+#define MAX_BRIGHT 64
+
+/**
+ * Fade to white or to black
+ */
+void fadeAll(bool fadeIn) {
+    if (fadeIn) {
+        for (int i = 0; i < MAX_BRIGHT; i++) {
+            analogWrite(RED, i);
+            analogWrite(BLUE, i);
+            analogWrite(GREEN, i);
+            delay(DELAY);
+        }
+    } else {
+        for (int i = MAX_BRIGHT; i >= 0; i--) {
+            analogWrite(RED, i);
+            analogWrite(BLUE, i);
+            analogWrite(GREEN, i);
+            delay(DELAY);
+        }
+    }
+}
+/**
+ * fades the color of the selected pin in or out
+ */
+void fadeColor(int colorPin, bool fadeIn) {
+    if (fadeIn) {
+        for (int i = 0; i < MAX_BRIGHT; i++) {
+            analogWrite(colorPin, i);
+            delay(DELAY);
+        }
+    } else {
+        for (int i = MAX_BRIGHT; i >= 0; i--) {
+            analogWrite(colorPin, i);
+            delay(DELAY);
+        }
+    }
+}
+
+/**
+ * Sets each pin to OUTPUT and turns them all off, then enables the default pin
+ */
 void setup() {
     // Setup the pins for output
     pinMode(RED, OUTPUT);
@@ -15,12 +59,28 @@ void setup() {
     digitalWrite(GREEN, LOW);
     digitalWrite(BLUE, LOW);
 
-}
-
-void setColor(int red, int green, int blue) {
-
+    // Setup RED to be lit to the correct brightness at startup
+    fadeColor(RED, true);
 }
 
 void loop() {
-// write your code here
+    // red -> green
+    fadeColor(RED, false);
+    fadeColor(GREEN, true);
+    delay(DELAY_BETWEEN_LIGHTS);
+
+    // green -> blue
+    fadeColor(GREEN, false);
+    fadeColor(BLUE, true);
+    delay(DELAY_BETWEEN_LIGHTS);
+
+    // blue -> while
+    fadeColor(BLUE, false);
+    fadeAll(true);
+    delay(DELAY_BETWEEN_LIGHTS);
+
+    // white -> red
+    fadeAll(false);
+    fadeColor(RED, true);
+    delay(DELAY_BETWEEN_LIGHTS);
 }
